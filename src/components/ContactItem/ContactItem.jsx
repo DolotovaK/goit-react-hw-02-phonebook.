b8849b92ecd599f9d891440bd9af6b50553fc2e8
Kateryna Dolotova <104764377+DolotovaK@users.noreply.github.com>
2022-12-07 20:02:15 +0200

    fix css

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -6,8 +6,8 @@ export const ContactItem = ({ contact: { name, number, id }, onDelete }) => {
       <p className={css.name}>{name}: {number}</p>
       <button
         type="button"
-        onClick={() => { onDelete(id) }}
         className={css.button}
+        onClick={() => { onDelete(id) }}
       >
         Delete
       </button>
